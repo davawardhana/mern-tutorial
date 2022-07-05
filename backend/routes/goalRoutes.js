f2076039d0e172326910c55dd0da3971aa46218c
davawardhana <wwrdhnxx00@gmail.com>
2022-07-05 21:39:03 +0700

    commit controller get

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { getGoals } = require('../controllers/goalController')
+
+router.get('/', getGoals)
 
 router.get('/',(req,res)=>{
     res.status(200).json({message: 'Get Goals'})
