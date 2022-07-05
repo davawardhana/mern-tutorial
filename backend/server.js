98b552ef219e7088f472601f4c41214ddde4e61b
davawardhana <wwrdhnxx00@gmail.com>
2022-07-05 20:20:00 +0700

    commit route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-console.log("hello bro")
+console.log("hello dava ganteng")
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(port, () => console.log('Server started on port $(port)'))
+app.get('/api/goals',(req,res)=>{ 
+    res.status(200).json({message: `Get Goals`})
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
 
