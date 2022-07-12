9c4c3b72601c3f73a55b9a561e5a405212afdf11
davawardhana <wwrdhnxx00@gmail.com>
2022-07-12 20:15:42 +0700

    commit Protect Goal Routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,27 +1,14 @@
-const express = require("express")
-const router = express.Router()
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+const express = require("express");
+const router = express.Router();
+const {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+  } = require("../controllers/goalController");
+  const { protect } = require("../middleware/authMiddleware");
 
-router.get('/',(req,res)=>{
-    res.status(200).json({message: 'Get Goals'})
-})
-router.get('/', getGoals)
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
-router.post('/',(req,res)=>{
-    res.status(200).json({message: 'Set Goal'})
-})
-router.post('/', setGoal)
-
-router.put('/:id',(req,res)=>{
-    res.status(200).json({message: `Update Goal ${req.params.id}`})
-})
-router.put('/:id', updateGoal)
-
-router.delete('/:id',(req,res)=>{
-    res.status(200).json({message: `Delete Goal ${req.params.id}`})
-})
-router.delete('/:id',deleteGoal)
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
-
-module.exports = router
+module.exports = router;
